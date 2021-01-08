1aaf014c45e957d2ae968914875667bb2275ab44
Rei Krom <mr.mygo@gmail.com>
2021-01-08 23:52:24 +0000

    add timer variable

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ const vgmUrl= 'https://www.currys.co.uk/gbuk/search-keywords/xx_xx_xx_xx_xx/rtx%
 const cardList = /#product10214421|product10214446|product10214425|product10214882|product10214434|product10214426|product10214430|product10216248|product10215051|product10216731|product10216247|product10219296|product10218689|product10219298|product10214421/g
 //how many times the script has ran
 let n = 0;
+// retry every x minutes
+const retryTime = 1 
 
 function watch(){ 
   (async function checkSite() {
@@ -30,7 +32,7 @@ function watch(){
     console.log("Try nr:", n)
     
   })();
-  setTimeout(watch, 60000);
+  setTimeout(watch, 60000*retryTime);
 }
 
 const whatsApp = (href) => {
